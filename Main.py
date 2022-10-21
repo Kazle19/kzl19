71837d4d8f2269e28b9bb0b7c7fc9f4a95cf7c6b
Kazle19 <115997960+Kazle19@users.noreply.github.com>
2022-10-21 19:43:18 +0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+$ pkg install bash
+$ pkg install python
+$ pkg install python2
+$ pkg install jq
+$ pkg install git
+$ git clone https://github.com/kzl19/spam-whatsapp
+$ bash spam-wa.sh
+& no target
